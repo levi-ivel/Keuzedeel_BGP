609164af994a47c4b19b9b3ad390651830ca6eec
levi <levi.kingma@gmail.com>
2024-06-18 10:30:35 +0200

    Feat: Next level functionality

---
+++ NextStage.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextStage : MonoBehaviour
+{
+private void OnTriggerEnter2D(Collider2D other)
+{
+int currentIndex = SceneManager.GetActiveScene().buildIndex;
+int nextSceneIndex = currentIndex + 1;
+
+if (other.tag == "Player")
+    {
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("No more scenes available.");
+        }
+    }
+}
+}
